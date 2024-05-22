6505051d039a160212e5fd9e24c095cc11f11c72
botirovdev <botirovdev7@gmail.com>
2024-05-22 19:15:19 +0500

    add swiper

--- src/components/Home/main/Movies.jsx
+++ src/components/Home/main/Movies.jsx
@@ -1,11 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useAllMovies } from "../../../contexts/Movies";
 import MovieSkeletonCard from "../../MovieCard/Skeleton/MovieCardSkeleton";
 import MovieCard from "../../MovieCard/MovieCard";
 import { Grid } from "@mui/material";
 import { t } from "i18next";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function Movies({ allMovies, language }) {
+  console.log("Movies component rendered");
+
   return (
     <section className="movies">
       <div className="container">
@@ -13,35 +22,62 @@ function Movies({ allMovies, language }) {
           <h1 className="movies-title">{t("MoviesTitle")}</h1>
           <div className="movies-movies">
             <div className="movies-cards">
-              {allMovies.isLoading ? (
-                <>
-                  <Grid item xs={12} sm={6} md={4} lg={3}>
-                    <MovieSkeletonCard />
-                  </Grid>
-                  <Grid item xs={12} sm={6} md={4} lg={3}>
-                    <MovieSkeletonCard />
-                  </Grid>
-                  <Grid item xs={12} sm={6} md={4} lg={3}>
-                    <MovieSkeletonCard />
-                  </Grid>
-                  <Grid item xs={12} sm={6} md={4} lg={3}>
-                    <MovieSkeletonCard />
-                  </Grid>
-                </>
-              ) : allMovies.movies.filter((m) => m.status.type === "movie")
-                  .length > 0 ? (
-                allMovies.movies
-                  .filter((m) => m.status.type === "movie")
-                  .map((movie) => (
-                    <MovieCard
-                      linkTo={`/movie/${movie._id}`}
-                      movie={movie}
-                      language={language}
-                    />
+              <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                breakpoints={{
+                  280: {
+                    slidesPerView: 1,
+                    spaceBetween: 70,
+                  },
+                  500: {
+                    slidesPerView: 2,
+                    spaceBetween: 70,
+                  },
+                  870: {
+                    slidesPerView: 3,
+                    spaceBetween: 70,
+                  },
+                  1200: {
+                    slidesPerView: 4,
+                    spaceBetween: 70,
+                  },
+                }}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                loop={true}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+              >
+                {allMovies.isLoading ? (
+                  Array.from({ length: 4 }).map((_, index) => (
+                    <SwiperSlide key={index}>
+                      <MovieSkeletonCard />
+                    </SwiperSlide>
                   ))
-              ) : (
-                <h1>{t("NoMovies")}</h1>
-              )}
+                ) : allMovies.movies.filter((m) => m.status.type === "movie").length > 0 ? (
+                  allMovies.movies
+                    .filter((m) => m.status.type === "movie")
+                    .map((movie) => {
+                      return(
+                        <SwiperSlide key={movie._id}>
+                          <MovieCard
+                            linkTo={`/movie/${movie._id}`}
+                            movie={movie}
+                            language={language}
+                          />
+                        </SwiperSlide>
+                      )
+                    })
+                ) : (
+                  <h1>{t("NoMovies")}</h1>
+                )}
+              </Swiper>
             </div>
           </div>
         </div>
