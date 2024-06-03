d11d2f65478b41a95ceb70f68cfed86d3cca9dfe
Usmon <usmon.umarovich.09@gmail.com>
2024-06-03 23:12:09 +0500

    pagination changed to mui pagination

--- src/components/utils/Pagination.jsx
+++ src/components/utils/Pagination.jsx
@@ -3,6 +3,7 @@ import ArrowForward from "@mui/icons-material/ArrowForward";
 import { IconButton, styled } from "@mui/material";
 import usePagination from "@mui/material/usePagination/usePagination";
 import React from "react";
+import PaginationMUI from "@mui/material/Pagination";
 
 function Pagination({ totalPosts, postsPerPage, setCurrentPage, currentPage }) {
   let pages = [];
@@ -22,94 +23,23 @@ function Pagination({ totalPosts, postsPerPage, setCurrentPage, currentPage }) {
   });
 
   return (
-    // <List>
-    //   {items.map(({ page, type, selected, ...item }, index) => {
-    //     let children = null;
-
-    //     if (type === "start-ellipsis" || type === "end-ellipsis") {
-    //       children = "…";
-    //     } else if (type === "page") {
-    //       children = (
-    //         <button
-    //           type="button"
-    //           style={{
-    //             fontWeight: selected ? "bold" : undefined,
-    //             background: selected ? "gold" : "transparent",
-    //             border: selected ? "1px solid gold" : "1px solid #fff",
-    //             color: selected ? "#000" : "#fff",
-    //             padding: "15px 20px",
-    //             borderRadius: "10px",
-    //             fontSize: "15px",
-    //           }}
-    //           {...item}
-    //         >
-    //           {page}
-    //         </button>
-    //       );
-    //     } else {
-    //       children = (
-    //         <button
-    //           style={{
-    //             border: "none",
-    //             color: "#fff",
-    //             padding: "15px 20px",
-    //             fontSize: "10px",
-    //           }}
-    //           type="button"
-    //           {...item}
-    //         >
-    //           {type == "previous" ? (
-    //             <ArrowBack />
-    //           ) : type == "next" ? (
-    //             <ArrowForward />
-    //           ) : (
-    //             ""
-    //           )}
-    //         </button>
-    //       );
-    //     }
-
-    //     return <li key={index}>{children}</li>;
-    //   })}
-    // </List>
     <List>
-      <IconButton
-        disabled={currentPage == 1}
-        onClick={() => setCurrentPage(currentPage - 1)}
-        color="inherit"
-      >
-        <ArrowBack />
-      </IconButton>
-      {pages.map((page, index) => {
-        return (
-          <>
-            <button
-              key={index}
-              onClick={() => setCurrentPage(page)}
-              type="button"
-              style={{
-                fontWeight: page == currentPage ? "bold" : undefined,
-                background: page == currentPage ? "gold" : "transparent",
-                border:
-                  page == currentPage ? "1px solid gold" : "1px solid #fff",
-                color: page == currentPage ? "#000" : "#fff",
-                padding: "15px 20px",
-                borderRadius: "10px",
-                fontSize: "15px",
-              }}
-            >
-              {page}
-            </button>
-          </>
-        );
-      })}
-      <IconButton
-        disabled={currentPage == pages.length}
-        onClick={() => setCurrentPage(currentPage + 1)}
-        color="inherit"
-      >
-        <ArrowForward />
-      </IconButton>
+      <PaginationMUI
+        sx={{
+          background: "white",
+          borderRadius: "10px",
+          padding: "5px 10px",
+          lineHeight: "50px",
+        }}
+        color="primary"
+        variant="outlined"
+        shape="circular"
+        size="large"
+        page={currentPage}
+        count={pages.length}
+        siblingCount={0}
+        onChange={(event, page) => setCurrentPage(page)}
+      />
     </List>
   );
 }
