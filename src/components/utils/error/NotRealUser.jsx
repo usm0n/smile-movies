0e66b7a9b5328a783d1325189123e71ddeac326c
Usmon <usmon.umarovich.09@gmail.com>
2024-06-10 08:08:17 +0500

    NotRealUser styles changed

--- src/components/utils/error/NotRealUser.jsx
+++ src/components/utils/error/NotRealUser.jsx
@@ -5,27 +5,34 @@ import { useUser } from "../../../contexts/User";
 import BuildIcon from "@mui/icons-material/Build";
 import logo from "../../../assets/images/logo.png";
 import { t } from "i18next";
+import PersonOffIcon from "@mui/icons-material/PersonOff";
 
 function NotRealUser() {
   const { logoutUser } = useUser();
   return (
     <div className="notrealuser">
-      <img className="notrealuser-img" src={logo} alt="" />
-      <h1 className="notrealuser-title">{t("YourNotRealUser")}</h1>
-      <p className="notrealuser-desc">{t("YourNotRealUserDesc")}</p>
-      <div className="notrealuser-btns">
-        <Link
-          className="notrealuser-btn"
-          onClick={() => logoutUser()}
-          to="/login"
-        >
-          <LoginIcon />
-          {t("MenuLoginText")}
-        </Link>
-        <Link className="notrealuser-btn" onClick={() => logoutUser()}>
-          <BuildIcon />
-          {t("fix")}
-        </Link>
+      <img className="network-logo" src={logo} alt="" />
+      <PersonOffIcon
+        sx={{ width: "150px", height: "150px" }}
+        className="network-icon"
+      />
+      <div className="network-content">
+        <h1 className="notrealuser-title">{t("YourNotRealUser")}</h1>
+        <p className="notrealuser-desc">{t("YourNotRealUserDesc")}</p>
+        <div className="notrealuser-btns">
+          <Link
+            className="notrealuser-btn"
+            onClick={() => logoutUser()}
+            to="/login"
+          >
+            <LoginIcon />
+            {t("MenuLoginText")}
+          </Link>
+          <Link className="notrealuser-btn" onClick={() => logoutUser()}>
+            <BuildIcon />
+            {t("fix")}
+          </Link>
+        </div>
       </div>
     </div>
   );
