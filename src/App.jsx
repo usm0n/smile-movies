d62272a1b5acce224e8ac2473cca8a9dfaa6f74d
Usmon <usmon.umarovich.09@gmail.com>
2024-06-12 16:49:38 +0500

    server alert deleted

--- src/App.jsx
+++ src/App.jsx
@@ -6,72 +6,18 @@ import Navbar from "./components/Navbar";
 import { useUser } from "./contexts/User";
 import NotRealUser from "./components/utils/error/NotRealUser";
 import VerifyEmail from "./components/utils/VerifyEmail";
-import { autoChangeLanguage, language } from "./utilities/defaultFunctions";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import { Snackbar } from "@mui/material";
+import { autoChangeLanguage } from "./utilities/defaultFunctions";
 import { useAllMovies } from "./contexts/Movies";
 import NetworkError from "./components/utils/error/NetworkError";
-import { Alert } from "@mui/joy";
-import Countdown from "react-countdown";
 import devtoolsDetect from "devtools-detect";
 import DevtoolsError from "./components/utils/error/DevtoolsError";
 
 function App() {
-  const { isNotConnected, allMovies } = useAllMovies();
+  const { isNotConnected } = useAllMovies();
   const { isRealUser, isLoggedIn, isVerified, isAdmin } = useUser();
-  const [ServerAlert, setServerAlert] = useState(true);
   const [isDevToolsOpen, setIsDevToolsOpen] = useState(devtoolsDetect.isOpen);
   const wrapper = (
     <div className="wrapper">
-      {allMovies.isLoading && (
-        <Countdown date={Date.now() + 3000}>
-          <Snackbar
-            anchorOrigin={{ vertical: "top", horizontal: "center" }}
-            open={ServerAlert == true}
-            onClose={() => setServerAlert(false)}
-          >
-            <Alert
-              endDecorator={
-                <IconButton onClick={() => setServerAlert(false)}>
-                  <CloseIcon />
-                </IconButton>
-              }
-              open={true}
-              color="warning"
-              variant="outlined"
-              sx={{
-                position: "absolute",
-                top: "70px",
-                width: "350px",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <div>
-                {language == "uz" ? (
-                  <>
-                    Saytimiz <Countdown date={Date.now() + 40000} /> ichida
-                    ishga tushadi
-                  </>
-                ) : language == "en" ? (
-                  <>
-                    Our site will be launched within{" "}
-                    <Countdown date={Date.now() + 40000} />
-                  </>
-                ) : (
-                  language == "ru" && (
-                    <>
-                      Наш сайт будет запущен в течение{" "}
-                      <Countdown date={Date.now() + 40000} />
-                    </>
-                  )
-                )}
-              </div>
-            </Alert>
-          </Snackbar>
-        </Countdown>
-      )}
       <Navbar />
       <Routes>
         {routes.map((item) => (
