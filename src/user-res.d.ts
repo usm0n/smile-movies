b46d3981fc4404838f8e98aa30879e28258daef1
Usmon Reyimberganov <usmonw@icloud.com>
2025-01-16 18:21:06 +0500

    service/api

---
+++ src/user-res.d.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: string;
+  firstname: string;
+  lastname?: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  isVerified: boolean;
+  isAdmin: boolean;
+  isBanned: boolean;
+}
+
+export interface Message {
+  message: string;
+}
+
+export interface CatchError {
+  message: string;
+  error: string;
+}
