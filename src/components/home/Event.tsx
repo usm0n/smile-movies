ac780df4a4a91c2b5d8410b48da00c1a607e773a
Usmon Reyimberganov <usmonw@icloud.com>
2025-01-29 23:16:51 +0500

    some style fixes

--- src/components/home/Event.tsx
+++ src/components/home/Event.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from "@mui/joy";
 import EventMC from "../cards/EventMC";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { ArrowForwardIos } from "@mui/icons-material";
 
 function Event({ eventTitle }: { eventTitle: string }) {
   return (
@@ -16,11 +17,18 @@ function Event({ eventTitle }: { eventTitle: string }) {
       }}
     >
       <Typography
+        endDecorator={<ArrowForwardIos />}
         level="h1"
         sx={{
           color: "rgb(255, 216, 77)",
           "@media (max-width: 800px)": {
             fontSize: "25px",
+          },
+          ":hover": {
+            cursor: "pointer",
+            opacity: 0.8,
+            transition: "all 0.2s ease-in-out",
+            textDecoration: "underline",
           },
         }}
       >
