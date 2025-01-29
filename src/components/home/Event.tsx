d7495945e537fa3fec43cf12d74e0c8ab05b3b7a
Usmon Reyimberganov <usmonw@icloud.com>
2025-01-29 23:11:36 +0500

    Home Movie Events

---
+++ src/components/home/Event.tsx
@@ -0,0 +1,92 @@
+import { Box, Typography } from "@mui/joy";
+import EventMC from "../cards/EventMC";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+
+function Event({ eventTitle }: { eventTitle: string }) {
+  return (
+    <Box
+      sx={{
+        paddingTop: "100px",
+        width: "90%",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        gap: 5,
+      }}
+    >
+      <Typography
+        level="h1"
+        sx={{
+          color: "rgb(255, 216, 77)",
+          "@media (max-width: 800px)": {
+            fontSize: "25px",
+          },
+        }}
+      >
+        {eventTitle}
+      </Typography>
+      <div>
+        <Swiper
+          style={
+            {
+              "--swiper-pagination-color": "#FFBA08",
+              "--swiper-pagination-bullet-inactive-color": "#999999",
+              "--swiper-pagination-bullet-inactive-opacity": "1",
+              "--swiper-pagination-bullet-size": "10px",
+              "--swiper-pagination-bullet-horizontal-gap": "5px",
+            } as any
+          }
+          slidesPerView={4}
+          spaceBetween={30}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+            },
+            700: {
+              slidesPerView: 2,
+              spaceBetween: 50,
+            },
+            1111: {
+              slidesPerView: 3,
+            },
+            1200: {
+              slidesPerView: 4,
+            },
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <EventMC />
+          </SwiperSlide>
+          <SwiperSlide>
+            <EventMC />
+          </SwiperSlide>
+          <SwiperSlide>
+            <EventMC />
+          </SwiperSlide>
+          <SwiperSlide>
+            <EventMC />
+          </SwiperSlide>
+          <SwiperSlide>
+            <EventMC />
+          </SwiperSlide>
+          <SwiperSlide>
+            <EventMC />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </Box>
+  );
+}
+
+export default Event;
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import Event from "../components/home/Event";
 import Header from "../components/home/Header";
 
 function HomeLayout() {
   return (
     <>
       <Header />
+      <Event eventTitle="Movies - Now Playing" />
+      <Event eventTitle="Movies - Popular" />
+      <Event eventTitle="Movies - Top Rated" />
+      <Event eventTitle="Movies - Upcoming" />
     </>
   );
 }
