49a6d40de935d008da9425ce3db34313b53e26b3
Usmon Reyimberganov <usmonw@icloud.com>
2025-05-22 00:13:00 +0500

    fixed google-login bug (replaced to button); removed unnecessary packages

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -36,11 +36,39 @@ import {
   reload,
 } from "../../utilities/defaults";
 import { useNavigate } from "react-router-dom";
-import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
+import { useGoogleLogin } from "@react-oauth/google";
 import { useUsers } from "../../context/Users";
 import { useOC } from "../../context/OC";
 import React from "react";
+import axios from "axios";
+
+const GoogleIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    width="24"
+    height="24"
+    viewBox="0 0 48 48"
+  >
+    <path
+      fill="#FFC107"
+      d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+    ></path>
+    <path
+      fill="#FF3D00"
+      d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+    ></path>
+    <path
+      fill="#4CAF50"
+      d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+    ></path>
+    <path
+      fill="#1976D2"
+      d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+    ></path>
+  </svg>
+);
 
 function Register() {
   const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false);
@@ -80,6 +108,47 @@ function Register() {
       [name]: name == "email" ? value.toLocaleLowerCase() : value,
     }));
   };
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      const userInfo = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        {
+          headers: {
+            Authorization: `Bearer ${tokenResponse.access_token}`,
+          },
+        }
+      );
+      const userData: GoogleUserResponse = userInfo.data;
+      login(
+        {
+          email: userData.email,
+          password: userData.sub,
+          deviceId: deviceId(),
+          deviceName: deviceName(),
+          deviceType: deviceType(),
+          deviceLocation: userValue.deviceLocation,
+        },
+        "google",
+        {
+          email: userData.email,
+          password: userData.sub,
+          firstname: userData.given_name,
+          lastname: userData.family_name,
+          isVerified: userData.email_verified,
+          profilePic: userData.picture,
+          deviceName: deviceName(),
+          deviceType: deviceType(),
+          deviceId: deviceId(),
+          loginType: "google",
+          deviceLocation: userValue.deviceLocation,
+        }
+      );
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -289,43 +358,14 @@ function Register() {
           </FormHelperText>
           <Divider>or</Divider>
           <Box gap={2} display={"flex"} flexDirection={"column"}>
-            <GoogleLogin
-              size="large"
-              text="signin_with"
-              theme={colorScheme === "dark" ? "filled_black" : "outline"}
-              onSuccess={(credentialResponse) => {
-                const decodedToken: GoogleUserResponse = jwtDecode(
-                  credentialResponse.credential || ""
-                );
-                login(
-                  {
-                    email: decodedToken.email,
-                    password: decodedToken.sub,
-                    deviceId: deviceId(),
-                    deviceName: deviceName(),
-                    deviceType: deviceType(),
-                    deviceLocation: userValue?.deviceLocation,
-                  },
-                  "google",
-                  {
-                    email: decodedToken.email,
-                    password: decodedToken.sub,
-                    firstname: decodedToken.given_name,
-                    lastname: decodedToken.family_name,
-                    isVerified: true,
-                    profilePic: decodedToken.picture,
-                    deviceId: deviceId(),
-                    deviceName: deviceName(),
-                    deviceType: deviceType(),
-                    loginType: "google",
-                    deviceLocation: userValue?.deviceLocation,
-                  }
-                );
-              }}
-              onError={() => console.log("Login Failed")}
-              useOneTap={!isLoggedIn}
-              auto_select={!isLoggedIn}
-            />
+            <Button
+              onClick={() => googleLogin()}
+              startDecorator={<GoogleIcon />}
+              variant="soft"
+              color="neutral"
+            >
+              Sign in with Google
+            </Button>
           </Box>
           {registerData?.isConflict && (
             <FormControl sx={{ width: "300px" }} color="danger">
